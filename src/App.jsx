c948012ad5b608a22259e14fce3082d524afb054
Gonzalo <gonzalo.estebarena@bootcamp-upgrade.com>
2022-09-05 21:09:05 +0200

    Library

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.scss';
+import Particle from './components/config/Particle';
+
+
+function App() {
+  return (
+    <div className="app">
+      <Particle/>
+      <h1>AYAYAY</h1>
+    </div>
+  );
+}
+
+export default App;
